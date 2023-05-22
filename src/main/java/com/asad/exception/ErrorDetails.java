4486a891b502fadf2c8704ae6a0c7bc609de01f3
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-22 09:53:56 +0600

    exception handle

---
+++ src/main/java/com/asad/exception/ErrorDetails.java
@@ -0,0 +1,16 @@
+package com.asad.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@Getter
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
---
+++ src/main/java/com/asad/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.asad.exception;
+
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ConfigDataResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ConfigDataResourceNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
