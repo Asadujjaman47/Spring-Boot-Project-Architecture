92eadd012e90300d8808403f9e83ddd6cbe6b54f
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-22 12:07:22 +0600

    Create UserMapper Class

--- src/main/java/com/asad/dto/UserDto.java
+++ src/main/java/com/asad/dto/UserDto.java
@@ -1,5 +1,14 @@
 package com.asad.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     private String firstName;
