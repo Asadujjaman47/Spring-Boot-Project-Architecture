1919fffc04501a1c65023872a920d0924f914b5e
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-22 09:33:34 +0600

    application.properties setup

---
+++ src/main/java/com/asad/SpringBootProjectArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.asad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectArchitectureApplication.class, args);
+	}
+
+}
